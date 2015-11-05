1c0ebaba10e99f7c5aa0c3ebe537cb0a563870b1
Calvin <calvin.chan.h@gmail.com>
2015-11-05 03:20:30 -0800

    FIXED USER -> PROFILES RELATIONSHIP
    Why was there a User has_many profiles relationship? Shouldn't each user only have one profile
    
    Changed the code so that it's a has_one relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :profiles, foreign_key: "user_id", dependent: :destroy
+  has_one :profile, foreign_key: "user_id", dependent: :destroy
   has_many :user_timeslots, foreign_key: "user_id", dependent: :destroy
   has_many :conversations, foreign_key: "sender_id"
   
