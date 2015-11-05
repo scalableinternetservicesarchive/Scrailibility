c9a8e01c46f41c43b60f50ebd893bf2d9cd1b5d0
Yining Li <clytze1116@gmail.com>
2015-11-05 10:41:59 -0800

    Slight Changes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,13 +5,16 @@ class Profile < ActiveRecord::Base
 	has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
 					  url: "/assets/profile/image/:id/:style/:basename.:extesnsion",
 					  path: ":rails_root/public/assets/profile/image/:id/:style/:basename.:extesnsion"
+    # attr_accessible :add1,:add2,:city,:state,:post_code,:latitude, :longitude
     geocoded_by :full_street_address   # can also be an IP address
-
+ 
+  
 
    def full_street_address
      [add1, add2, city, state, post_code].compact.join(', ')
    end
-    after_validation :geocode          # auto-fetch coordinates
+    after_validation :geocode ,:if => :add1_changed?
+    # ,:if => :address_changed?      # auto-fetch coordinates
 
   	validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
   	validates_attachment_size :photo, less_than: 5.megabytes
