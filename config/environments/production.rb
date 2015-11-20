8667a66c9106c6fa03b84f0f690ee0f11a615ef0
TANGCHIJIANGBIN <Tom@wifi-131-179-36-250.host.ucla.edu>
2015-11-20 14:55:06 -0800

    cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
   #use dalli as cache store
-  config.cache_store = :dalli_store, nil, { :namespace => NAME_OF_RAILS_APP, :expires_in => 1.day, :compress => true,  :pool_size => 5 }
+  config.cache_store = :dalli_store, nil, { :namespace => 'Scrailibility', :expires_in => 1.day, :compress => true,  :pool_size => 5 }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
