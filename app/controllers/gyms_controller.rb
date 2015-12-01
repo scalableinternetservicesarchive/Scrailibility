77cd0683d98dcf470923ac5586310aaa1d285048
TANGCHIJIANGBIN <tangchijiangbin@gmail.com>
2015-11-30 17:28:19 -0800

    using week Etag

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -2,6 +2,7 @@ class GymsController < ApplicationController
   def nearby_gym
     @latitude = current_user.profile.latitude
     @longitude = current_user.profile.longitude
+    fresh_when([@latitude, @longitude])
   end
 
 end
