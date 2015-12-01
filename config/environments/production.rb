612129467fa202a399ea61748ee079612df71309
TANGCHIJIANGBIN <tangchijiangbin@gmail.com>
2015-12-01 11:59:14 -0800

    missing ,

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
       },
       :fog_directory => 'scalableinternetservices',
       :path => 'Scrailibility/profiles/images/:class/:attachment/:id/:style/:filename',
-      :url => "profiles/images/:class/:attachment/:id/:style/:filename"
+      :url => "profiles/images/:class/:attachment/:id/:style/:filename",
       :default_url => '/images/avatar/missing_:style.gif'
     }
 end
