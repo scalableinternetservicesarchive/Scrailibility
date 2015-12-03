4813e2648eac1a2b7b62d2af210094810765a207
TANGCHIJIANGBIN <tangchijiangbin@gmail.com>
2015-12-03 12:10:42 -0800

    Revert "test discoverly caching"
    This reverts commit 24d1092d3a2906a32bc84f7b9d50d8182f85807b.

--- app/controllers/discoveries_controller.rb
+++ app/controllers/discoveries_controller.rb
@@ -1,5 +1,5 @@
 class DiscoveriesController < ApplicationController
-	# before_action :authenticate_user!
+	before_action :authenticate_user!
 
 
 	def gym_people
@@ -20,45 +20,26 @@ class DiscoveriesController < ApplicationController
 	#need to change here! currently just show all the registered users
 	#how to verify the identify of current user
 	def show
-		# if current_user.profile.nil?
-		# 	redirect_to new_profile_path
-		# 	return
-		# end
-		if ! current_user
-			@people = Rails.cache.fetch("Los Angeles_90024_matches") do
-				timeslots = current_user.timeslots
-				nearby = Discoveries.instance.findNearbyUsers(current_user.profile.id, 20)
-				people = Hash.new
-				people.default = nil
-				for timeslot in timeslots
-					users = timeslot.users.includes(:profile).where(id: nearby).limit(9)
-					for user in users
-						if (!people[user.profile.id] and user.profile.id != current_user.profile.id)
-							people[user.profile.id] = user.profile
-						end
+		if current_user.profile.nil?
+			redirect_to new_profile_path
+			return
+		end
+		# Rails.cache.delete("#{current_user.profile.city}_#{current_user.profile.post_code}_matches")
+		@people = Rails.cache.fetch("#{current_user.profile.city}_#{current_user.profile.post_code}_matches") do
+			timeslots = current_user.timeslots
+			nearby = Discoveries.instance.findNearbyUsers(current_user.profile.id, 20)
+			people = Hash.new
+			people.default = nil
+			for timeslot in timeslots
+				users = timeslot.users.includes(:profile).where(id: nearby).limit(9)
+				for user in users
+					if (!people[user.profile.id] and user.profile.id != current_user.profile.id)
+						people[user.profile.id] = user.profile
 					end
 				end
-				Rails.cache.write("Los Angeles_90024_matches", people, expires_in: 2.minutes)
-				people
 			end
-		else
-			# Rails.cache.delete("#{current_user.profile.city}_#{current_user.profile.post_code}_matches")
-			@people = Rails.cache.fetch("#{current_user.profile.city}_#{current_user.profile.post_code}_matches") do
-				timeslots = current_user.timeslots
-				nearby = Discoveries.instance.findNearbyUsers(current_user.profile.id, 20)
-				people = Hash.new
-				people.default = nil
-				for timeslot in timeslots
-					users = timeslot.users.includes(:profile).where(id: nearby).limit(9)
-					for user in users
-						if (!people[user.profile.id] and user.profile.id != current_user.profile.id)
-							people[user.profile.id] = user.profile
-						end
-					end
-				end
-				Rails.cache.write("#{current_user.profile.city}_#{current_user.profile.post_code}_matches", people, expires_in: 2.minutes)
-				people
-			end
+			Rails.cache.write("#{current_user.profile.city}_#{current_user.profile.post_code}_matches", people, expires_in: 2.minutes)
+			people
 		end
 		fresh_when(@people.values)
 	end
